100c7f709cf196e409b0218ac439e155233810e9
gbsoares12 <gbsoares12@gmail.com>
2019-03-13 17:11:10 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ButtonModule} from 'primeng/components/button/button';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {TableModule} from 'primeng/table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
